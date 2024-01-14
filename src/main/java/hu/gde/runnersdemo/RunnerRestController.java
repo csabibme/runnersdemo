a810be993457c51a421bad8598bb0d905ddff4ea
csabibme <36829084+csabibme@users.noreply.github.com>
2024-01-15 00:16:34 +0100

    shoesize avgPace chgshoe chgshoe_template

--- src/main/java/hu/gde/runnersdemo/RunnerRestController.java
+++ src/main/java/hu/gde/runnersdemo/RunnerRestController.java
@@ -111,4 +111,6 @@ public class RunnerRestController {
             return ResponseEntity.notFound().build();
         }
     }
+
+
 }
--- src/main/java/hu/gde/runnersdemo/ShoeNameEntity.java
+++ src/main/java/hu/gde/runnersdemo/ShoeNameEntity.java
@@ -43,4 +43,6 @@ public class ShoeNameEntity {
     public void setShoeName(String shoeName) {
         this.shoeName = shoeName;
     }
+
+
 }
